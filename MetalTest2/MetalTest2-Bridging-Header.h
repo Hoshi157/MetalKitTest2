3872a46f74def2beea69e5c4693384be21346c39
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-07-21 11:08:43 +0900

    Initial Commit

---
+++ MetalTest2/MetalTest2-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  MetalTest2-Bridging-Header.h
+//  MetalTest2
+//
+//  Created by 福山帆士 on 2020/07/21.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
